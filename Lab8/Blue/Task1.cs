3b2aadb636a4c870f4b1ce204502eefa121c6688
KADYK007 <kadykov07@yandex.ru>
2026-03-29 15:38:56 +0300

    Update Task1.cs

--- Lab8/Blue/Task1.cs
+++ Lab8/Blue/Task1.cs
@@ -2,5 +2,96 @@
 {
     public class Task1
     {
+        public class Response
+        {
+            // Поля 
+            private string _name;
+            protected int _votes;
+            
+            // Свойства
+            public string Name => _name;
+            public int Votes => _votes;
+            
+
+            // Конструктор
+            public Response(string name)
+            {
+                _name = name;
+                _votes = 0;
+            }
+            
+            // Метод
+            public virtual int CountVotes(Response[] responses)
+            {
+                int count = 0;
+                for (int i = 0; i < responses.Length; i++)
+                {
+                    if (responses[i].Name == _name)
+                    {
+                        count++;
+                    }
+                }
+                
+                for (int i = 0; i < responses.Length; i++)
+                {
+                    if (responses[i].Name == _name)
+                    {
+                        responses[i]._votes = count;
+                    }
+                }
+
+                return count;
+            }
+
+            public virtual void Print()
+            {
+                Console.WriteLine($"Имя: {_name}\nГолосов: {_votes}");
+            }
+        }
+
+        public class HumanResponse : Response
+        {
+            private string _surname;
+
+            public string Surname => _surname;
+            
+            public HumanResponse(string name, string surname) : base(name)
+            {
+                _surname = surname;
+            }
+
+            public override int CountVotes(Response[] responses)
+            {
+                int count = 0;
+                for (int i = 0; i < responses.Length; i++)
+                {
+                    if (responses[i] is HumanResponse human)
+                    {
+                        if (human.Name == Name && human.Surname == _surname)
+                        {
+                            count++;
+                        }
+                    }
+                }
+                
+                for (int i = 0; i < responses.Length; i++)
+                {
+                    if (responses[i] is HumanResponse human)
+                    {
+                        if (human.Name == Name && human.Surname == _surname)
+                        {
+                            human._votes = count;
+                        }
+                    }
+                }
+
+                return count;
+            }
+            
+            public override void Print()
+            {
+                Console.WriteLine($"Имя: {Name}\nГолосов: {Votes}");
+            }
+        }
     }
 }

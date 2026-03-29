d852f2d2e439c6c82f2df4b9b50cd95c8889bac7
dashausova <m2502203@edu.misis.ru>
2026-03-29 13:29:19 +0300

    Update Task4.cs

--- Lab8/White/Task4.cs
+++ Lab8/White/Task4.cs
@@ -1,74 +1,57 @@
-namespace Lab8.White
+using System;
+
+namespace Lab7.White
 {
     public class Task4
     {
         public class Human
         {
-            //поля
-            private string _name;
-            private string _surname;
+            protected string _name;
+            protected string _surname;
 
-
-
-            //свойства
             public string Name => _name;
             public string Surname => _surname;
 
             public Human(string name, string surname)
             {
-                //проинициализировать все нестатик поля
                 _name = name;
                 _surname = surname;
-
-
             }
+
             public virtual void Print()
             {
-                Console.WriteLine(_name);
-                Console.WriteLine(_surname);
+                Console.WriteLine($"Имя: {_name}");
+                Console.WriteLine($"Фамилия: {_surname}");
             }
-
         }
+
         public class Participant : Human
         {
-            //поля 
             private double[] _scores;
-
+            private int _scoresCount;
             private static int _count;
-            //свойства
 
             public static int Count => _count;
-
-
-
 
             public double[] Scores
             {
                 get
                 {
-                    if (_scores == null || _scores.Length == 0)
-                        return new double[0];
-
-
-                    double[] copy = new double[_scores.Length];
-                    for (int i = 0; i < _scores.Length; i++)
+                    double[] result = new double[_scoresCount];
+                    for (int i = 0; i < _scoresCount; i++)
                     {
-                        copy[i] = _scores[i];
+                        result[i] = _scores[i];
                     }
-                    return copy;
-
-
+                    return result;
                 }
-
             }
+
             public double TotalScore
             {
                 get
                 {
-                    if (_scores == null || _scores.Length == 0)
-                        return 0;
                     double sum = 0;
-                    for (int i = 0; i < _scores.Length; i++)
+                    for (int i = 0; i < _scoresCount; i++)
                     {
                         sum += _scores[i];
                     }
@@ -76,62 +59,46 @@ namespace Lab8.White
                 }
             }
 
-
-
-            // Статический конструктор 
             static Participant()
             {
                 _count = 0;
             }
 
-
-
-            //конструктор
             public Participant(string name, string surname) : base(name, surname)
             {
-                _scores = new double[0];
+                _scores = new double[100];
+                _scoresCount = 0;
                 _count++;
             }
 
-
             public void PlayMatch(double result)
             {
-
-                if (result == 0 || result == 0.5 || result == 1)
+                if (_scoresCount < _scores.Length)
                 {
-                    if (_scores == null)
-                        _scores = new double[0];
-                    Array.Resize(ref _scores, _scores.Length + 1);
-                    _scores[_scores.Length - 1] = result;
+                    _scores[_scoresCount] = result;
+                    _scoresCount++;
                 }
-
-
             }
+
             public static void Sort(Participant[] array)
             {
-                if (array == null || array.Length == 0)
-                    return;
-
-                // Пузырьковая сортировка
-                for (int i = 0; i < array.Length; i++)
+                for (int i = 0; i < array.Length - 1; i++)
                 {
-                    for (int j = 1; j < array.Length; j++)
+                    for (int j = 0; j < array.Length - 1 - i; j++)
                     {
-                        if (array[j - 1].TotalScore < array[j].TotalScore)
+                        if (array[j].TotalScore < array[j + 1].TotalScore)
                         {
-                            (array[j - 1], array[j]) = (array[j], array[j - 1]);
+                            (array[j], array[j + 1]) = (array[j + 1], array[j]);
                         }
                     }
                 }
             }
+
             public override void Print()
             {
                 base.Print();
-                Console.WriteLine(Count);
-                Console.WriteLine(_scores);
-                Console.WriteLine(TotalScore);
+                Console.WriteLine($"Очки за матчи: {TotalScore}");
             }
         }
-
     }
 }

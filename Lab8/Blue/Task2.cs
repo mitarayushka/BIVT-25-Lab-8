ebb593fb7c174c4041f8b9b528696062245c909c
Hiron2305 <mrHiron@yandex.ru>
2026-03-29 22:06:45 +0300

    initial

--- Lab8/Blue/Task2.cs
+++ Lab8/Blue/Task2.cs
@@ -1,6 +1,151 @@
-﻿namespace Lab8.Blue
+﻿using System;
+
+namespace Lab8.Blue
 {
     public class Task2
     {
+        public struct Participant
+        {
+            private string _name;
+            private string _surname;
+            private int[][] _marks;
+            private int _totalScore;
+
+            public string Name => _name;
+            public string Surname => _surname;
+            public int[][] Marks => _marks;
+            public int TotalScore => _totalScore;
+
+            public Participant(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _marks = new int[0][];
+                _totalScore = 0;
+            }
+
+            public void Jump(int[] marks)
+            {
+                if (_marks == null)
+                    _marks = new int[0][];
+
+                Array.Resize(ref _marks, _marks.Length + 1);
+                _marks[_marks.Length - 1] = marks;
+
+                if (marks != null)
+                {
+                    foreach (var m in marks)
+                    {
+                        _totalScore += m;
+                    }
+                }
+            }
+
+            public static void Sort(Participant[] participants)
+            {
+                if (participants == null) return;
+                Array.Sort(participants, (p1, p2) => p2.TotalScore.CompareTo(p1.TotalScore));
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"{Name} {Surname}: {TotalScore}");
+            }
+        }
+
+        public abstract class WaterJump
+        {
+            private string _name;
+            private int _bank;
+            private Participant[] _participants;
+
+            public string Name => _name;
+            public int Bank => _bank;
+            public Participant[] Participants => _participants;
+
+            public abstract double[] Prize { get; }
+
+            protected WaterJump(string name, int bank)
+            {
+                _name = name;
+                _bank = bank;
+                _participants = new Participant[0];
+            }
+
+            public void Add(Participant p)
+            {
+                if (_participants == null)
+                    _participants = new Participant[0];
+
+                Array.Resize(ref _participants, _participants.Length + 1);
+                _participants[_participants.Length - 1] = p;
+            }
+
+            public void Add(Participant[] p)
+            {
+                if (p == null || p.Length == 0) return;
+                if (_participants == null)
+                    _participants = new Participant[0];
+
+                int oldLength = _participants.Length;
+                Array.Resize(ref _participants, oldLength + p.Length);
+                Array.Copy(p, 0, _participants, oldLength, p.Length);
+            }
+        }
+
+        public class WaterJump3m : WaterJump
+        {
+            public WaterJump3m(string name, int bank) : base(name, bank) { }
+
+            public override double[] Prize
+            {
+                get
+                {
+                    if (Participants == null || Participants.Length < 3)
+                        return null;
+
+                    return new double[]
+                    {
+                        Bank * 0.50,
+                        Bank * 0.30,
+                        Bank * 0.20
+                    };
+                }
+            }
+        }
+
+        public class WaterJump5m : WaterJump
+        {
+            public WaterJump5m(string name, int bank) : base(name, bank) { }
+
+            public override double[] Prize
+            {
+                get
+                {
+                    if (Participants == null || Participants.Length < 3)
+                        return null;
+
+                    int winnersCount = Participants.Length / 2;
+                    if (winnersCount < 3) winnersCount = 3;
+                    if (winnersCount > 10) winnersCount = 10;
+
+                    double nPercent = 20.0 / winnersCount;
+                    double bonus = Bank * (nPercent / 100.0);
+
+                    double[] prizes = new double[winnersCount];
+
+                    for (int i = 0; i < winnersCount; i++)
+                    {
+                        prizes[i] = bonus;
+                    }
+
+                    if (winnersCount >= 1) prizes[0] += Bank * 0.40;
+                    if (winnersCount >= 2) prizes[1] += Bank * 0.25;
+                    if (winnersCount >= 3) prizes[2] += Bank * 0.15;
+
+                    return prizes;
+                }
+            }
+        }
     }
 }
--- Lab8/Blue/Task5.cs
+++ Lab8/Blue/Task5.cs
@@ -1,6 +1,206 @@
+using System;
+
 namespace Lab8.Blue
 {
     public class Task5
     {
+        public class Sportsman
+        {
+            private string _name;
+            private string _surname;
+            private int _place;
+
+            public string Name => _name;
+            public string Surname => _surname;
+            public int Place => _place;
+
+            public Sportsman(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _place = 0;
+            }
+
+            public void SetPlace(int place)
+            {
+                _place = place;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"{Name} {Surname} - Ìוסעמ: {Place}");
+            }
+        }
+
+        public abstract class Team
+        {
+            private string _name;
+            private Sportsman[] _sportsmen;
+
+            public string Name => _name;
+
+            public Sportsman[] Sportsmen
+            {
+                get
+                {
+                    if (_sportsmen == null) return new Sportsman[0];
+                    return (Sportsman[])_sportsmen.Clone();
+                }
+            }
+
+            public int TotalScore
+            {
+                get
+                {
+                    if (_sportsmen == null) return 0;
+                    int sum = 0;
+                    foreach (var s in _sportsmen)
+                    {
+                        if (s.Place >= 1 && s.Place <= 5)
+                        {
+                            sum += (6 - s.Place);
+                        }
+                    }
+                    return sum;
+                }
+            }
+
+            public int TopPlace
+            {
+                get
+                {
+                    if (_sportsmen == null || _sportsmen.Length == 0) return 18;
+
+                    int min = int.MaxValue;
+                    bool hasValid = false;
+                    foreach (var s in _sportsmen)
+                    {
+                        if (s.Place > 0)
+                        {
+                            if (s.Place < min) min = s.Place;
+                            hasValid = true;
+                        }
+                    }
+                    return hasValid ? min : 0;
+                }
+            }
+
+            public Team(string name)
+            {
+                _name = name;
+                _sportsmen = new Sportsman[0];
+            }
+
+            public void Add(Sportsman sportsman)
+            {
+                if (sportsman == null) return;
+                if (_sportsmen == null) _sportsmen = new Sportsman[0];
+                Array.Resize(ref _sportsmen, _sportsmen.Length + 1);
+                _sportsmen[_sportsmen.Length - 1] = sportsman;
+            }
+
+            public void Add(Sportsman[] sportsmen)
+            {
+                if (sportsmen == null) return;
+                foreach (var s in sportsmen)
+                {
+                    Add(s);
+                }
+            }
+
+            public static void Sort(Team[] array)
+            {
+                if (array == null) return;
+                Array.Sort(array, (t1, t2) =>
+                {
+                    if (t1 == null && t2 == null) return 0;
+                    if (t1 == null) return 1;
+                    if (t2 == null) return -1;
+
+                    int cmp = t2.TotalScore.CompareTo(t1.TotalScore);
+                    if (cmp == 0)
+                    {
+                        return t1.TopPlace.CompareTo(t2.TopPlace);
+                    }
+                    return cmp;
+                });
+            }
+
+            protected abstract double GetTeamStrength();
+
+            public static Team GetChampion(Team[] teams)
+            {
+                if (teams == null || teams.Length == 0) return null;
+
+                Team champion = null;
+                double maxStrength = -1;
+
+                foreach (var t in teams)
+                {
+                    if (t != null)
+                    {
+                        double str = t.GetTeamStrength();
+                        if (str > maxStrength)
+                        {
+                            maxStrength = str;
+                            champion = t;
+                        }
+                    }
+                }
+
+                return champion;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"{Name} - Ñקוע: {TotalScore}, Ëףקרוו לוסעמ: {TopPlace}");
+            }
+        }
+
+        public class ManTeam : Team
+        {
+            public ManTeam(string name) : base(name) { }
+
+            protected override double GetTeamStrength()
+            {
+                var sports = Sportsmen;
+                if (sports.Length == 0) return 0;
+
+                double sum = 0;
+                foreach (var s in sports)
+                {
+                    sum += s.Place;
+                }
+
+                if (sum == 0) return 0;
+                double avg = sum / sports.Length;
+
+                return 100.0 / avg;
+            }
+        }
+
+        public class WomanTeam : Team
+        {
+            public WomanTeam(string name) : base(name) { }
+
+            protected override double GetTeamStrength()
+            {
+                var sports = Sportsmen;
+                if (sports.Length == 0) return 0;
+
+                double sum = 0;
+                double prod = 1;
+
+                foreach (var s in sports)
+                {
+                    sum += s.Place;
+                    if (s.Place != 0) prod *= s.Place;
+                }
+
+                if (prod == 0) return 0;
+
+                return 100.0 * sum * sports.Length / prod;
+            }
+        }
     }
 }

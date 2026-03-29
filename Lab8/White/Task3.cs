de6e27685711291d34063b4590755b2914ad3304
dashausova <m2502203@edu.misis.ru>
2026-03-29 12:07:26 +0300

    Update Task3.cs

--- Lab8/White/Task3.cs
+++ Lab8/White/Task3.cs
@@ -1,120 +1,77 @@
+
+
 namespace Lab8.White
 {
-    public class Task2
+    public class Task3
     {
-        public class Participant
+        public class Student
         {
-            //создаем поля
+            //поля
             private string _name;
             private string _surname;
-            private double _firstjump;
-            private double _secondjump;
+            protected int _skipped;
+            protected int[] _marks;
 
-
-            private static readonly double _standard;
-
-
-
-
-            //свойства 
+            //свойства
             public string Name => _name;
             public string Surname => _surname;
-            public double FirstJump => _firstjump;
-            public double SecondJump => _secondjump;
+            public int Skipped => _skipped;
 
 
-            public double BestJump
+            public double AverageMark
             {
                 get
                 {
-                    if (_firstjump == 0 && _secondjump == 0)
+                    if (_marks == null || _marks.Length == 0)
                         return 0;
-                    else if (_firstjump != 0 && _secondjump == 0)
-                        return FirstJump;
-                    else if (_secondjump != 0 && _firstjump == 0)
-                        return SecondJump;
-                    else
-                        return Math.Max(_firstjump, _secondjump);
-
+                    double average = 0;
+                    for (int i = 0; i < _marks.Length; i++)
+                    {
+                        average += _marks[i];
+                    }
+                    average = average / _marks.Length;
+                    return average;
                 }
             }
-
-            //участника IsPassed – прошел он норматив или нет.
-            public bool IsPassed
-            {
-                get
-                {
-                    return BestJump >= _standard;
-                }
-            }
-
-            static Participant()
-            {
-                _standard = 3.0;
-            }
-
-            //конструктор из 2 строк поля
-            public Participant(string name, string surname)
+            //конструктор
+            public Student(string name, string surname)
             {
                 _name = name;
                 _surname = surname;
-                _firstjump = 0;
-                _secondjump = 0;
 
             }
-            //конструктор из 4 строк поля
-            public Participant(string name, string surname, double firstjump, double secondjump)
+
+
+            //принимает объект студента и копирует его значения в соответствующие поля.
+            protected Student(Student student)
             {
-                _name = name;
-                _surname = surname;
-                _firstjump = firstjump;
-                _secondjump = secondjump;
+                _name = student._name;
+                _surname = student._surname;
+                _skipped = student._skipped;
+                _marks = student._marks;
             }
 
 
 
             //метод
-            public static Participant[] GetPassed(Participant[] participants)
+            public void Lesson(int mark)
             {
-                if (participants == null || participants.Length == 0)
-                    return new Participant[0];
-                int count = 0;
-                for (int i = 0; i < participants.Length; i++)
+                if (mark == 0)
                 {
-                    if (participants[i].IsPassed)
-                        count++;
+                    _skipped++;
                 }
-                Participant[] result = new Participant[count];
-
-                int k = 0;
-                for (int i = 0; i < participants.Length; i++)
+                else
                 {
-                    if (participants[i].IsPassed)
-                    {
-                        result[k++] = participants[i];
-                    }
-                }
-                return result;
+                    //если массив не сущ создаем пустой
+                    if (_marks == null)
+                        _marks = new int[0];
 
-            }
-
-
-
-
-
-            public void Jump(double result)
-            {
-                if (_firstjump == 0)
-                {
-                    _firstjump = result;
-                }
-                else if (_secondjump == 0)
-                {
-                    _secondjump = result;
+                    Array.Resize(ref _marks, _marks.Length + 1);
+                    _marks[_marks.Length - 1] = mark;
                 }
 
             }
-            public static void Sort(Participant[] array)
+            public static void SortBySkipped(Student[] array)
             {
                 if (array == null || array.Length == 0)
                     return;
@@ -122,10 +79,9 @@ namespace Lab8.White
                 {
                     for (int j = 1; j < array.Length; j++)
                     {
-                        if (array[j - 1].BestJump < array[j].BestJump)
+                        if (array[j - 1].Skipped < array[j].Skipped)
                         {
                             (array[j - 1], array[j]) = (array[j], array[j - 1]);
-
                         }
                     }
                 }
@@ -134,10 +90,64 @@ namespace Lab8.White
             {
                 Console.WriteLine(_name);
                 Console.WriteLine(_surname);
-                Console.WriteLine(_firstjump);
-                Console.WriteLine(_secondjump);
-                Console.WriteLine(BestJump);
+                Console.WriteLine(_skipped);
+                Console.WriteLine(_marks);
+                Console.WriteLine(AverageMark);
             }
+
         }
+        public class Undergraduate : Student
+        {
+
+            public Undergraduate(string name, string surname) : base(name, surname)
+            {
+
+            }
+
+            public Undergraduate(Student student) : base(student)
+            {
+
+            }
+
+            //получить новую оценку по предмету вместо одного пропуска. Если пропусков нет, то заменить
+            //имеющуюся оценку 2 на новую оценку
+            public void WorkOff(int mark)
+            {
+                if (_skipped > 0)
+                {
+                    //есть пропуски, заменяем один пропуск на оценку
+                    _skipped--;
+                    Lesson(mark);
+
+                }
+                else
+
+                {
+                    // пропусков нет, ищем оценку 2 и заменяем её
+                    for (int i = 0; i < _marks.Length; i++)
+                    {
+                        if (_marks[i] == 2)
+                        {
+                            _marks[i] = mark;
+                            return;
+
+
+                        }
+                    }
+                }
+
+            }
+            public new void Print()
+            {
+
+                Console.WriteLine(_skipped);
+                Console.WriteLine(_marks);
+                Console.WriteLine(WorkOff);
+
+            }
+
+        }
+
+
     }
 }
